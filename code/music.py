c8f94bb466448b236b5c095951bc1994cccfbd2b
MouseSun <3026098675@qq.com>
2019-07-23 10:42:54 +0800

    爬虫音乐下载器

--- code/music.py
+++ code/music.py
@@ -10,7 +10,8 @@ import json
 class Spider(object):
 	starname = 'AJonnyCool'
 	# url = 'http://king.junyuewl.com/api.php?callback=jQuery111307253625129728498_1563796139283'
-	url_ku_gou = 'http://king.junyuewl.com/api.php?callback=jQuery111307253625129728498_1563797077768'
+	# url_ku_gou = 'http://king.junyuewl.com/api.php?callback=jQuery111307253625129728498_1563797077768'
+	url_ku_gou = 'http://king.junyuewl.com/api.php?'
 	data = {'types': 'search', 'count': '666', 'source': 'kugou', 'pages': '1', 'name': starname}
 	header = {'Host':'king.junyuewl.com','Referer':'https://y.qq.com/portal/search.html','Content-Type':'application/x-www-form-urlencoded','User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0'}
 	music_info =[]
@@ -18,9 +19,12 @@ class Spider(object):
 
 		str = input()
 		self.starname = str
+		self.data['name'] = str
 		response=requests.post(self.url_ku_gou,data=self.data,headers=self.header)
-		jsonres=re.findall(r'jQuery111307253625129728498_1563797077768\((.*)\)',response.text,re.S)
-		res = json.loads(''.join(jsonres))
+		# print(response.text)
+		# jsonres=re.findall(r'jQuery111307253625129728498_1563797077768\((.*)\)',response.text,re.S)
+		# res = json.loads(''.join(jsonres))
+		res = json.loads(response.text)
 		self.music_info =[]
 		self.music_info = res
 		# print(res)
@@ -40,16 +44,22 @@ class Spider(object):
 		# print(res['url']
 		# print(response.text)
 		if len(res['url'])>0:
-			print(res['url'])
-			# res = requests.get(res['url'])
-			# path = 'F:/python/音乐下载/'+self.starname
-			# ispath=os.path.exists(path)
-			# if not ispath:
-			# 	os.mkdir(path)
-			# with open('F:/python/音乐下载/'+self.starname+'/'+music['name']+'.mp3','wb') as fw:
-			# 	fw.write(res.content)
-			# 	fw.close()
-			# 	print(music['name']+'.mp3'+'下载完成！！！')
+			# print(res['url'])
+			path = 'F:/python/音乐下载/'+self.starname
+			ispath=os.path.exists(path)
+			if not ispath:
+				os.mkdir(path)
+			name = 'F:/python/音乐下载/'+self.starname+'/'+music['name']+'.mp3'
+			isname=os.path.exists(name)
+			# 如果不存在，才下载
+			if not isname:
+				res = requests.get(res['url'])
+				with open(name,'wb') as fw:
+					fw.write(res.content)
+					fw.close()
+					print(music['name']+'.mp3'+'下载完成！！！')
+			else:
+				print(name+'  已经存在-----')
 	def getmusicnum(self):
 		return len(self.music_info)
 	
